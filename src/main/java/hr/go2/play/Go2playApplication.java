be049d97baa62a5e168196e7f53b3daf2128a43a
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2019-10-04 18:13:41 +0200

    Initial commit

---
+++ src/main/java/hr/go2/play/Go2playApplication.java
@@ -0,0 +1,13 @@
+package hr.go2.play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Go2playApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Go2playApplication.class, args);
+	}
+
+}
