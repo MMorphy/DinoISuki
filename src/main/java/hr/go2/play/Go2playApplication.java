89fccc9a9d329afbb052c215ae5058ec68ec8570
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2019-10-09 15:53:21 +0200

    Enabled JPA repositories

--- src/main/java/hr/go2/play/Go2playApplication.java
+++ src/main/java/hr/go2/play/Go2playApplication.java
@@ -2,8 +2,10 @@ package hr.go2.play;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("hr.go2.play.repositories")
 public class Go2playApplication {
 
 	public static void main(String[] args) {
