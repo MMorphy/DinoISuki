3b31e79feedf1422fddb3c528397c14ee5b36513
Valentino Kasner <valentino.kasner@ericsson.com>
2019-10-11 10:52:13 +0200

    CameraRepository - find & delete by name field

--- src/main/java/hr/go2/play/repositories/CameraRepository.java
+++ src/main/java/hr/go2/play/repositories/CameraRepository.java
@@ -1,9 +1,15 @@
 package hr.go2.play.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hr.go2.play.entities.Camera;
 
 public interface CameraRepository extends JpaRepository<Camera, Long> {
+	
+	public List<Camera> findByName(String name);
+	
+	public void deleteByName(String name);
 
 }
