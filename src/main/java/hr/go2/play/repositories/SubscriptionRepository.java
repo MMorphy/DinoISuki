99845f7377eb9fb9d34d977c6acbfcf41b753f10
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2019-10-12 12:20:42 +0200

    Subscription repository - without tests

--- src/main/java/hr/go2/play/repositories/SubscriptionRepository.java
+++ src/main/java/hr/go2/play/repositories/SubscriptionRepository.java
@@ -1,9 +1,17 @@
 package hr.go2.play.repositories;
 
+import java.util.Collection;
+import java.util.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hr.go2.play.entities.Subscription;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long>{
 
+	Collection<Subscription> findByValidFrom (Date validFrom);
+
+	Collection<Subscription> findByValidTo (Date validTo);
+
+	Collection<Subscription> findBySubscriptionType_Name (String name);
 }
