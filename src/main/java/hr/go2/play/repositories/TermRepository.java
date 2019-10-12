719da91901cc0c756382b837ae73030a56320a3b
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2019-10-12 12:41:59 +0200

    TermRepository - without tests

--- src/main/java/hr/go2/play/repositories/TermRepository.java
+++ src/main/java/hr/go2/play/repositories/TermRepository.java
@@ -1,9 +1,24 @@
 package hr.go2.play.repositories;
 
+import java.util.Collection;
+import java.util.Date;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hr.go2.play.entities.Term;
 
 public interface TermRepository extends JpaRepository<Term, Long>{
 
+	Collection<Term> findByDate(Date date);
+
+	Collection<Term> findByDateAndTimeFrom(Date date, Date timeFrom);
+
+	Collection<Term> findByDateAndTimeFromAndAvailable(Date date, Date timeFrom, boolean available);
+
+	Collection<Term> findByDateAndTimeTo(Date date, Date timeTo);
+
+	Collection<Term> findByDateAndTimeToAndAvailable(Date date, Date timeTo, boolean available);
+
+	Optional<Term> findByVideos_Location(String path);
 }
