4b74e7d2655efe07f907fbf4187bcdd8da1249b9
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2019-10-12 12:07:34 +0200

    Role Repository added - without tests

--- src/main/java/hr/go2/play/repositories/RoleRepository.java
+++ src/main/java/hr/go2/play/repositories/RoleRepository.java
@@ -1,9 +1,20 @@
 package hr.go2.play.repositories;
 
+import java.util.Collection;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hr.go2.play.entities.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long>{
 
+	Collection<Role> findByName(String name);
+
+	Optional<Role> findByLocations_Name (String name);
+
+	Optional<Role> findByLocations_Address (String address);
+
+	Collection<Role> findByUsers_Username (String username);
 }
+
