ab4e042b045176e30fb8cf4316340acf0d513e3d
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2019-10-12 11:39:53 +0200

    Field repository - without tests

--- src/main/java/hr/go2/play/repositories/FieldRepository.java
+++ src/main/java/hr/go2/play/repositories/FieldRepository.java
@@ -1,9 +1,17 @@
 package hr.go2.play.repositories;
 
+import java.util.Collection;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hr.go2.play.entities.Field;
 
 public interface FieldRepository extends JpaRepository<Field, Long>{
 
+	Collection<Field> findBySport(String sport);
+
+	Optional<Field> findByCameras_Name(String name);
+
+	Collection<Field> findByTerms_Available(boolean availability);
 }
