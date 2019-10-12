92ef5d255501e56e646221354f6263a94d8dcf1b
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2019-10-12 11:49:41 +0200

    Location repository - without test

--- src/main/java/hr/go2/play/repositories/LocationRepository.java
+++ src/main/java/hr/go2/play/repositories/LocationRepository.java
@@ -1,9 +1,23 @@
 package hr.go2.play.repositories;
 
+import java.util.Collection;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hr.go2.play.entities.Location;
+import hr.go2.play.entities.User;
+import hr.go2.play.entities.WorkingHours;
 
 public interface LocationRepository extends JpaRepository<Location, Long>{
 
+	public Optional<Location> findByAddress(String address);
+
+	public Optional<Location> findByAddressLike(String address);
+
+	public Optional<Location> findByContactUser(User contactUser);
+
+	public Collection<Location> findByWorkingHours(WorkingHours wh);
+
+	public Optional<Location> findByFields_Sport(String sport);
 }
