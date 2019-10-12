427e492396ae55b0de53d95ba606392e80b0c262
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2019-10-12 12:22:27 +0200

    SubscriptionType repository - added

--- src/main/java/hr/go2/play/repositories/SubscriptionTypeRepository.java
+++ src/main/java/hr/go2/play/repositories/SubscriptionTypeRepository.java
@@ -1,9 +1,12 @@
 package hr.go2.play.repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hr.go2.play.entities.SubscriptionType;
 
 public interface SubscriptionTypeRepository extends JpaRepository<SubscriptionType, Long>{
 
+	Collection<SubscriptionType> findByName(String name);
 }
