298bd4b25854b8fe1b411bb6cbf1cd12dc1578ae
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2019-10-12 13:09:09 +0200

    VideoRepository - without tests

--- src/main/java/hr/go2/play/repositories/VideoRepository.java
+++ src/main/java/hr/go2/play/repositories/VideoRepository.java
@@ -1,9 +1,12 @@
 package hr.go2.play.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hr.go2.play.entities.Video;
 
 public interface VideoRepository extends JpaRepository<Video, Long>{
 
+	Optional<Video> findByLocation(String location); 
 }
