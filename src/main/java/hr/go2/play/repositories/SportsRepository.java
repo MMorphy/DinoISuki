0c67bced6aafe5279f27ab717d9bef453d834e0a
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2019-10-12 12:15:39 +0200

    Sports repository - without tests

--- src/main/java/hr/go2/play/repositories/SportsRepository.java
+++ src/main/java/hr/go2/play/repositories/SportsRepository.java
@@ -1,9 +1,12 @@
 package hr.go2.play.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hr.go2.play.entities.Sports;
 
 public interface SportsRepository extends JpaRepository<Sports, Long>{
 
+	Optional<Sports> findByName(String name);
 }
