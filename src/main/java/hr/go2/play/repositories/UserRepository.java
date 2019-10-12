c09022bcae090b9f92e84e48e2ab70f8777edcf7
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2019-10-12 13:00:26 +0200

    UserRepository - without tests

--- src/main/java/hr/go2/play/repositories/UserRepository.java
+++ src/main/java/hr/go2/play/repositories/UserRepository.java
@@ -1,9 +1,22 @@
 package hr.go2.play.repositories;
 
+import java.util.Collection;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import hr.go2.play.entities.Term;
 import hr.go2.play.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	Collection<User> findByRoles_name(String name);
+
+	Collection<User> findByEnabled(boolean enabled);
+
+	Collection<User> findByReservedTerms(Term reservedTerm);
+
+	Collection<User> findByLikedSports_Name(String name);
+
+	Optional<User> findByUsername(String username);
 }
