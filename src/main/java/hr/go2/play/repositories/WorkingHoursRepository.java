c34c14d8dab6df7e14317f580d5bd4d14a0cdb76
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2019-10-12 13:09:40 +0200

    WorkingHoursRepository - without tests

--- src/main/java/hr/go2/play/repositories/WorkingHoursRepository.java
+++ src/main/java/hr/go2/play/repositories/WorkingHoursRepository.java
@@ -1,9 +1,17 @@
 package hr.go2.play.repositories;
 
+import java.util.Collection;
+import java.util.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hr.go2.play.entities.WorkingHours;
 
 public interface WorkingHoursRepository extends JpaRepository<WorkingHours, Long>{
 
+	Collection<WorkingHours> findByFromTime(Date fromTime);
+
+	Collection<WorkingHours> findByToTime(Date toTime);
+
+	Collection<WorkingHours> findByFromTimeAndToTime(Date fromTime, Date toTime);
 }
