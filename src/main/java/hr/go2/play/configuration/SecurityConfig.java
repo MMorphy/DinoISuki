15805547e784e6467e56407e3161e460b918a8c8
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2019-10-18 11:19:57 +0200

    Initial security config

---
+++ src/main/java/hr/go2/play/configuration/SecurityConfig.java
@@ -0,0 +1,18 @@
+package hr.go2.play.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/**").permitAll();
+		http.cors().disable();
+		http.csrf().disable();
+	}
+}
