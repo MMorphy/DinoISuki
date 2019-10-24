3cff5f9d28d269c8d0fe7bbd4ef21a5fddd0eddb
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2019-10-24 19:51:43 +0200

    AngularJS FE navbar skeleton

--- src/main/resources/public/infrastructure/app.component.js
+++ src/main/resources/public/infrastructure/app.component.js
@@ -4,10 +4,12 @@
 vod.component('base',{
 
     template:`
+    <div class="container">
     <h1> Test</h1>
     <nav-bar></nav-bar>
     <div class="col-lg-12" ui-view></div>
-    `,
+    
+    </div>`,
     controller:function(){
     }
 })
--- src/main/resources/public/infrastructure/nav-bar.component.js
+++ src/main/resources/public/infrastructure/nav-bar.component.js
@@ -1,18 +1,61 @@
-tvzStore.component('navBar', {
-    template:`
-            <nav class="navbar navbar-default" role="navigation">
-              <div class="navbar-collapse collapse">
-                <ul class="nav navbar-nav navbar-left">
-                    <li><a class="nav-link" ui-sref="home">Home</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                 </ul>
-              </div>
-            </nav>
-   
+vod.component('navBar', {
+    template: `
+<nav class="navbar nav-fill w-100 navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">go2play</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Locations</a>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Sports
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Football</a>
+          <a class="dropdown-item" href="#">Tennis</a>
+          <div class="dropdown-divider"></div>
+        </div>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">Videos</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav mr-right">
+       <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Administration
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Location</a>
+          <a class="dropdown-item" href="#">Global</a>
+          <div class="dropdown-divider"></div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Register</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">My Profile</a>
+      </li>
+    </ul>
+  </div>
+</nav>
    `,
-    controller:function () {
+    controller: function () {
 
     },
-    controllerAs:'c'
+    controllerAs: 'c'
 })
