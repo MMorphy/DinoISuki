1baf4644aba884ef97a2095b6d5cfc2eafa43eaf
Valentino Kasner <valentino.kasner@ericsson.com>
2019-11-10 19:13:37 +0100

    Notification & Team repositories

---
+++ src/main/java/hr/go2/play/repositories/NotificationRepository.java
@@ -0,0 +1,28 @@
+package hr.go2.play.repositories;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import hr.go2.play.entities.Notification;
+import hr.go2.play.entities.NotificationStatus;
+import hr.go2.play.entities.User;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+	
+	Collection<Notification> findByCreatedAt(Date createdAt);
+	
+	Collection<Notification> findBySourceUser(User srcUser);
+	
+	Collection<Notification> findByDestUser(User destUser);
+	
+	Collection<Notification> findByMessage(String  message);
+	
+	Collection<Notification> findByStatus(NotificationStatus status);
+	
+	Collection<Notification> findBySourceUserAndDestUser(User srcUser, User destUser);
+	
+	Collection<Notification> findBySourceUserAndStatus(User srcUser, NotificationStatus status);
+
+}
---
+++ src/main/java/hr/go2/play/repositories/TeamRepository.java
@@ -0,0 +1,17 @@
+package hr.go2.play.repositories;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import hr.go2.play.entities.Role;
+import hr.go2.play.entities.Team;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+	
+	Optional<Team> findByName(String name);
+	
+	Collection<Team> findByUsers_Username (String username);
+
+}
