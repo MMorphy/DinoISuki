87e6b062ead6677fd16063e74b4a18a259a056c1
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2019-11-10 18:14:45 +0100

    Added DayType entity and added it to workingHours

---
+++ src/main/java/hr/go2/play/entities/DayType.java
@@ -0,0 +1,46 @@
+package hr.go2.play.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="day_type")
+public class DayType {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String type;
+
+	public DayType() {
+	}
+
+	public DayType(Long id, String type) {
+		super();
+		this.id = id;
+		this.type = type;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
--- src/main/java/hr/go2/play/entities/WorkingHours.java
+++ src/main/java/hr/go2/play/entities/WorkingHours.java
@@ -2,11 +2,15 @@ package hr.go2.play.entities;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -27,15 +31,19 @@ public class WorkingHours {
 	@Column(nullable = false, name = "to_time")
 	private Date toTime;
 
-	public WorkingHours() {
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "day_type_id")
+	private DayType dayType;
 
+	public WorkingHours() {
 	}
 
-	public WorkingHours(Long id, Date fromTime, Date toTime) {
+	public WorkingHours(Long id, Date fromTime, Date toTime, DayType dayType) {
 		super();
 		this.id = id;
 		this.fromTime = fromTime;
 		this.toTime = toTime;
+		this.dayType = dayType;
 	}
 
 	public Long getId() {
@@ -60,6 +68,14 @@ public class WorkingHours {
 
 	public void setToTime(Date toTime) {
 		this.toTime = toTime;
+	}
+
+	public DayType getDayType() {
+		return dayType;
+	}
+
+	public void setDayType(DayType dayType) {
+		this.dayType = dayType;
 	}
 
 }
