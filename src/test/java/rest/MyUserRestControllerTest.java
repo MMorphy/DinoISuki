bffc0b1c5acf04a4f5a0385ef7f45929fcc19f53
Leo Jakus <leo.jakus@gmail.com>
2020-01-16 16:41:03 +0100

    Added user update tests

--- src/test/java/rest/MyUserRestControllerTest.java
+++ src/test/java/rest/MyUserRestControllerTest.java
@@ -7,16 +7,18 @@ import static rest.RestControllerTestUtility.*;
 
 public class MyUserRestControllerTest {
 
-    private String fakeJson="{\"userDto\":{\"id\":\"9999999\",\"createdAt\":\"2019/11/19 00:00:00\",\"dateOfBirth\":\"1995/10/10\",\"username\":\"defaultUser\",\"password\":\"newPass\"},\"contactInfoDto\":{\"id\":\"1\",\"telephoneNumber\":\"0800091091\",\"email\":\"newdefaultuser101@gmail.com\"}}";
-    private String brokenJson="{\"createdAt\":\"broken\",\"dateOfBirth\":\"broken\",\"username\":\"broken\",\"password\":\"broken\"}";
-    private String myUserJson= "{\"userDto\":{\"id\":\"1\",\"createdAt\":\"2019/11/19 00:00:00\",\"dateOfBirth\":\"1995/10/10\",\"username\":\"defaultUser\",\"password\":\"newPass\"},\"contactInfoDto\":{\"id\":\"1\",\"telephoneNumber\":\"0800091091\",\"email\":\"newdefaultuser101@gmail.com\"}}";
+    private String fakeJson="{\"userDto\":{\"dateOfBirth\":\"2025/10/10\",\"username\":\"zdrafko\",\"password\":\"newPass\"},\"contactInfoDto\":{\"telephoneNumber\":\"666666\",\"email\":\"malformedgmail@com\"}}";
+    private String brokenJson="{\"userDto\":{\"dateOfBirth\":\"\",\"username\":\"\",\"password\":\"\"},\"contactInfoDto\":{\"telephoneNumber\":\"\",\"email\":\"\"}}";
+    private String myUserJson= "{\"userDto\":{\"dateOfBirth\":\"1995/10/10\",\"username\":\"defaultUser\",\"password\":\"newPass\"},\"contactInfoDto\":{\"telephoneNumber\":\"060222\",\"email\":\"newdefaultuser101@gmail.com\"}}";
     private String myUserURL="/api/myUser/update/user";
     private String successMsg = "User updated!";
     private String token;
 
     @BeforeMethod
     public void setup(){
+
         token = RestControllerTestUtility.setup();
+        System.out.println("Token je: " + token);
     }
 
     @AfterMethod
@@ -27,26 +29,39 @@ public class MyUserRestControllerTest {
     @Test
     public void updateUserTest(){
         int result = postWithToken(myUserURL, myUserJson, token);
-        assertTrue(result == statusSuccess);
+        assertTrue(result == statusSuccess, "Failed to update user as expected, response code not correct! Response code: " + result);
     }
 
     @Test
     public void updateUserWithBodyTest(){
 
         String result = postWithTokenBody(myUserURL, myUserJson, token);
-        assertTrue(result.contains(successMsg));
+        assertTrue(result.contains(successMsg), "Failed to update user as expected, response message not correct: " + result);
     }
 
     @Test
     public void updateFalseUserTest(){
         int result = postWithToken(myUserURL, fakeJson, token);
-        assertTrue(result == statusSuccess); //TODO: change
+        assertTrue(result == statusFail, "False user update failed with wrong code: " + result);
     }
 
     @Test
     public void invalidUpdateUserJsonTest(){
         int result = postWithToken(myUserURL, brokenJson, token);
-        assertTrue(result == statusSuccess);
+        assertTrue(result == statusFail, "Invalid user update failed with wrong code: " + result);
+    }
+
+    @Test
+    public void noResponseUpdateUserTest(){
+        int result = postWithToken(myUserURL, "", token);
+        assertTrue(result == statusFail, "Empty user update failed with wrong code: " + result);
+    }
+
+    @Test
+    public void doubleUpdateUserTest(){
+        postWithToken(myUserURL, myUserJson, token);
+        int result = postWithToken(myUserURL, myUserJson, token);
+        assertTrue(result == statusSuccess, "Double user update failed with wrong code: " + result); //TODO: change this when functionality changes
     }
 
     @Test
