726e7f6619d7fc97bb75900335800fbec3aaec8a
Neven Brezac <neven.brezac@gmail.com>
2020-02-26 14:42:11 +0100

    BE Fetch profile photo

--- src/main/java/hr/go2/play/controllers/UserManagement.java
+++ src/main/java/hr/go2/play/controllers/UserManagement.java
@@ -1,5 +1,8 @@
 package hr.go2.play.controllers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
@@ -15,8 +18,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.annotation.Secured;
@@ -29,6 +35,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -50,7 +57,7 @@ import hr.go2.play.util.Commons;
 @RequestMapping("/api/user")
 @Secured("ROLE_USER")
 public class UserManagement {
-	
+
 	@Value("${application.users.profile-photo-location}") String profilePhotoLocation;
 
 	//Login
@@ -58,7 +65,7 @@ public class UserManagement {
 	//Register
 	ModelMapper mapper = new ModelMapper();
 	Logger logger = LoggerFactory.getLogger(UserManagement.class);
-	
+
 	@Autowired
     AuthenticationManager authenticationManager;
 
@@ -70,25 +77,25 @@ public class UserManagement {
 
     @Autowired
     ContactInformationRepository contactInfoRepo;
-    
+
     @Autowired
     RoleRepository roleRepo;
 
     @Autowired
     private UserDetailsService userDetailsService;
-    
+
     @Autowired
     private UserServiceImpl userService;
-    
+
     @Autowired
     private ContactInformationServiceImpl contactInfoService;
 
     @Autowired
     private Commons commons;
- 
+
     /**
      * Desc: User login
-     * { 
+     * {
           "username":"test4",
           "password":"test4"
        }
@@ -161,7 +168,7 @@ public class UserManagement {
         userDetailsService.saveUser(user, contactInfo);
         return new ResponseEntity<String>(commons.JSONfyReturnMessage("User created successfully"), HttpStatus.CREATED);
     }
-    
+
     /**
      * Desc: Update existing user
      * JSON body example:
@@ -193,11 +200,11 @@ public class UserManagement {
         	return new ResponseEntity<String>(commons.JSONfyReturnMessage("Invalid JSON"), HttpStatus.BAD_REQUEST);
     	}
 		ContactInformation contactInfo = user.getContactInfo();
-		
+
 		ContactInformation updatedContactInfo = new ContactInformation();
 		updatedContactInfo.setEmail(userContactInfoDto.getContactInfoDto().getEmail());
 		updatedContactInfo.setTelephoneNumber(userContactInfoDto.getContactInfoDto().getTelephoneNumber());
-		
+
 		User updatedUser = new User();
 		updatedUser.setCreatedAt(user.getCreatedAt());
 		updatedUser.setDateOfBirth(userContactInfoDto.getUserDto().getDateOfBirth());
@@ -206,17 +213,17 @@ public class UserManagement {
 		updatedUser.setRoles(user.getRoles());
 		updatedUser.setContactInfo(updatedContactInfo);
 		updatedUser.setUsername(username);
-		
+
 		try {
 			contactInfoService.updateContactInformation(contactInfo.getId(), updatedContactInfo);
 			userDetailsService.updateUser(user.getId(), updatedUser, updatedContactInfo);
 		} catch (DataIntegrityViolationException e) {
 			return new ResponseEntity<String>(commons.JSONfyReturnMessage("Invalid JSON"), HttpStatus.BAD_REQUEST);
 		}
-		
+
 		return new ResponseEntity<String>(commons.JSONfyReturnMessage("User updated!"), HttpStatus.CREATED);
 	}
-	
+
 	@GetMapping("/getUser/{username}")
 	public ResponseEntity<User> getUser(@PathVariable String username) {
 		if(userRepo.existsByUsername(username)) {
@@ -230,16 +237,16 @@ public class UserManagement {
 			return new ResponseEntity<User>(new User(), HttpStatus.NOT_ACCEPTABLE);
 		}
 	}
-	
+
 	// profile photo - add
-	@PostMapping(value = "/uploadProfilePhoto", consumes = "multipart/form-data")
+	@PostMapping(value = "/uploadProfilePhoto", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public ResponseEntity<?> uploadProfilePhoto(@RequestParam(name = "profilePhoto", required = false) MultipartFile profilePhoto, @RequestParam(name = "username") String username) {
 		logger.debug("/api/user/uploadProfilePhoto Started");
-		
+
 		if (username == null || username.isBlank()) {
             return new ResponseEntity<String>(commons.JSONfyReturnMessage("No username provided"), HttpStatus.BAD_REQUEST);
         }
-		
+
 		// does this user already have a profile photo?
 		User user = userService.findUserByUsername(username);
 		if(user != null && user.getProfilePhoto() != null) {
@@ -255,19 +262,19 @@ public class UserManagement {
 		} else {
 			return new ResponseEntity<String>(commons.JSONfyReturnMessage("Unable to find user"), HttpStatus.BAD_REQUEST);
 		}
-		
+
 		// if no profile photo provided, delete the existing one
 		if(profilePhoto == null || profilePhoto.isEmpty()) {
 			// already deleted in previous step
 			return new ResponseEntity<String>(commons.JSONfyReturnMessage("Profile photo successfuly removed"), HttpStatus.OK);
 		}
-		
+
         try {
         	// store the file
         	byte[] bytes = profilePhoto.getBytes();
             Path path = Paths.get(profilePhotoLocation + "/" + username + "_" + profilePhoto.getOriginalFilename());
             Files.write(path, bytes);
-            
+
             // store the data about the file info to database
             user.setProfilePhoto(username + "_" + profilePhoto.getOriginalFilename());
             userService.updateUser(user.getId(), user);
@@ -278,8 +285,42 @@ public class UserManagement {
         	logger.error("Unable to store file", e);
         	return new ResponseEntity<String>(commons.JSONfyReturnMessage("Unable to store file. " + e.getMessage()), HttpStatus.BAD_REQUEST);
         }
-        
+
         return new ResponseEntity<String>(commons.JSONfyReturnMessage("Successfully uploaded - " + profilePhoto.getOriginalFilename()), HttpStatus.OK);
+	}
+
+	// profile photo - fetch
+	@GetMapping(value = "/getProfilePhoto")
+	@ResponseBody
+	public ResponseEntity<?> getProfilePhoto(@RequestParam(name = "username") String username) {
+		logger.debug("/api/user/getProfilePhoto Started");
+		// does this user have a profile photo?
+		User user = userService.findUserByUsername(username);
+		if(user != null) {
+			if(user.getProfilePhoto() != null) {
+			File profilePhoto = new File(profilePhotoLocation + "/" + user.getProfilePhoto());
+	        HttpHeaders headers = new HttpHeaders();
+	        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+	        headers.add("Pragma", "no-cache");
+	        headers.add("Expires", "0");
+	        headers.setContentLength(profilePhoto.length());
+	        headers.setContentType(MediaType.parseMediaType("application/octet-stream"));
+
+			InputStreamResource resource = null;
+			try {
+				resource = new InputStreamResource(new FileInputStream(profilePhoto));
+			} catch (FileNotFoundException e) {
+				logger.error("Unable to find profile photo", e);
+				return new ResponseEntity<String>(commons.JSONfyReturnMessage("Unable to find profile photo"), HttpStatus.BAD_REQUEST);
+			}
+
+			return new ResponseEntity<Object>(resource, headers, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<String>(commons.JSONfyReturnMessage("No profile photo found"), HttpStatus.NOT_FOUND);
+			}
+		} else {
+			return new ResponseEntity<String>(commons.JSONfyReturnMessage("Unable to find user"), HttpStatus.BAD_REQUEST);
+		}
 	}
 
 }
