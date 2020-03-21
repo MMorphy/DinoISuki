19206addca92be4a0c947ef3ef71c9ac1aa7f020
AndreaKurek <andrea_kurek@yahoo.com>
2020-03-21 18:41:24 +0100

    podaci o prodajnom mjestu update

--- src/main/js/components/about/SalesPointDetails.tsx
+++ src/main/js/components/about/SalesPointDetails.tsx
@@ -13,27 +13,43 @@ export default class SalesPointDetails extends React.Component<{}, {}> {
                 <Accordion.Collapse eventKey="0">
                     <Card.Body>
                         <Card.Text>
-                            Go2Play j.d.o.o.
-                            <br/>
-                            <br/>
-                            Adresa: Hrgovici 35, Kneza Ljudevita Posavskog 36b, 10 000 Zagreb
-                            <br/>
-                            <br/>
-                            OIB: 14509637799
-                            <br/>
-                            <br/>
-                            Porezni broj: *9999*
-                            <br/>
-                            <br/>
-                            Tehnički kontakt:
-                            Dino *prezime* - +385 91 619 9912, Ivan Šušnja - +385 91 160 0010
-                            <br/>
-                            <br/>
-                            Računovodstveni kontakt: Snježana Frulja +385 98 817 471
-                            <br/>
-                            <br/>
-                            Email adresa: dinoh8@outlook.com, info@ibas-idea.hr
-                            {/*Maticni br posl.sub.- 5201411*/}
+                            <div className="row">
+                                <div className="column sales-point-details-column-width">
+                                    Ime tvrtke:
+                                    <br/>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;Go2Play j.d.o.o.
+                                    <br/>
+                                    <br/>
+                                    OIB:
+                                    <br/>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;14509637799
+                                    <br/>
+                                    <br/>
+                                    Matični broj poslovnog subjekta:
+                                    <br/>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;5201411
+                                </div>
+                                <div className="column sales-point-details-column-width">
+                                    Tehnički kontakt:
+                                    <br/>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;Dino Hadžić - +385 91 619 9912
+                                    <br/>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;Ivan Šušnja - +385 91 160 0010
+                                    <br/>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;Hrgovići 35, 10 000 Zagreb
+                                    <br/>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;dinoh8@outlook.com
+                                    <br/>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;info@ibas-idea.hr
+                                    <br/>
+                                    <br/>
+                                    Računovodstveni kontakt:
+                                    <br/>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;Snježana Frulja - +385 98 817 471
+                                    <br/>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;Kneza Ljudevita Posavskog 36b, 10 000 Zagreb
+                                </div>
+                            </div>
                         </Card.Text>
                     </Card.Body>
                 </Accordion.Collapse>
