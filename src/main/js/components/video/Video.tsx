d8279f0bf89c08a705c8edbd1b65fe53225e469c
AndreaKurek <andrea_kurek@yahoo.com>
2020-03-29 20:57:50 +0200

    horizontal scroll with one video fixed

--- src/main/js/components/video/Video.tsx
+++ src/main/js/components/video/Video.tsx
@@ -11,11 +11,29 @@ interface VideoProps {
 }
 
 export default class Video extends React.Component<VideoProps, {}> {
+    private player: any = null;
+
+    ref = (player: any) => {
+        this.player = player
+    };
+
     render() {
         return (
             <div className="video-player-inline">
-                <ReactPlayer url={`https://localhost:8443/api/video/getVideo/${this.props.videoName}`} light={this.props.isVideoThumbnail} playing={this.props.playing} controls={true} width={this.props.width} height={this.props.height} onReady={this.props.onStart}/>
+                <ReactPlayer url={`https://localhost:8443/api/video/getVideo/${this.props.videoName}`}
+                             light={this.props.isVideoThumbnail}
+                             playing={this.props.playing}
+                             controls={true}
+                             width={this.props.width}
+                             height={this.props.height}
+                             onReady={this.props.onStart}
+                             onSeek={seconds => this.handleSeek(seconds)}
+                             ref={this.ref}/>
             </div>
         );
+    }
+
+    private handleSeek(seconds: number) {
+        this.player.seekTo(seconds, "seconds");
     }
 }
