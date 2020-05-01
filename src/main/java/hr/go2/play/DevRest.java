c098c64139073af757514dbac05ecfd467e109ef
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2020-05-01 16:08:40 +0200

    Added subscription dummy rest
    * Call post with this path /api/dev/generateSub
    * If successfull the output will be:
    	{"message":"Successfully made subscriptions for all users!"}

--- src/main/java/hr/go2/play/DevRest.java
+++ src/main/java/hr/go2/play/DevRest.java
@@ -1,9 +1,11 @@
 package hr.go2.play;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +22,9 @@ import hr.go2.play.entities.DayType;
 import hr.go2.play.entities.Field;
 import hr.go2.play.entities.Location;
 import hr.go2.play.entities.Role;
+import hr.go2.play.entities.Subscription;
+import hr.go2.play.entities.SubscriptionType;
+import hr.go2.play.entities.User;
 import hr.go2.play.entities.WorkingHours;
 import hr.go2.play.impl.CameraServiceImpl;
 import hr.go2.play.impl.ContactInformationServiceImpl;
@@ -27,7 +32,10 @@ import hr.go2.play.impl.DayTypeServiceImpl;
 import hr.go2.play.impl.FieldServiceImpl;
 import hr.go2.play.impl.LocationServiceImpl;
 import hr.go2.play.impl.RoleServiceImpl;
+import hr.go2.play.impl.SubscriptionServiceImpl;
+import hr.go2.play.impl.UserServiceImpl;
 import hr.go2.play.impl.WorkingHoursServiceImpl;
+import hr.go2.play.services.SubscriptionTypeService;
 import hr.go2.play.util.Commons;
 
 @RestController
@@ -39,6 +47,8 @@ public class DevRest {
 	private static final int NO_OF_FIELDS = 3;
 
 	private static final int NO_OF_CAMERAS = 3;
+
+	private static final long MILIS_IN_A_DAY = TimeUnit.DAYS.toMillis(1);
 
 	@Autowired
 	private LocationServiceImpl locService;
@@ -62,17 +72,86 @@ public class DevRest {
 	private Commons commons;
 
 	@Autowired
+	private UserServiceImpl userService;
+
+	@Autowired
 	private RoleServiceImpl roleService;
+
+	@Autowired
+	private SubscriptionServiceImpl subService;
+
+	@Autowired
+	private SubscriptionTypeService stService;
 
 	@PostMapping("/generateDummy")
 	public ResponseEntity<String> generateDummyData() {
 		return new ResponseEntity<String>(generateDummyDataLogic(), HttpStatus.ACCEPTED);
 	}
 
+	@PostMapping("/generateSub")
+	public ResponseEntity<String> generateSubDummyData() {
+		return new ResponseEntity<String>(generateSubscriptionDummyDataLogic(), HttpStatus.ACCEPTED);
+	}
+
 	private String generateDummyDataLogic() {
 		generateTestLocation();
-		addRoles();
+//		addRoles();
 		return commons.JSONfyReturnMessage("Successfully made PSQL entries!");
+	}
+
+	private String generateSubscriptionDummyDataLogic() {
+		generateSubscriptions();
+		return commons.JSONfyReturnMessage("Successfully made subscriptions for all users!");
+	}
+	private void generateSubscriptions() {
+		if (stService.findAllSubscriptionTypes().isEmpty()) {
+			generateSubscriptionTypes();
+		}
+		List<User> users = userService.findAllUsers();
+		for (User u : users) {
+			Subscription activeSub = new Subscription();
+			activeSub.setSubscriptionType(stService.findSubscriptionTypeByName("Half Year").get(0));
+			activeSub.setValid(true);
+			activeSub.setValidFrom(new Date());
+			activeSub.setValidTo(new Date(Instant.now().toEpochMilli() + (MILIS_IN_A_DAY * 180)));
+			subService.saveSubscription(activeSub);
+
+			Subscription inactiveSub = new Subscription();
+			inactiveSub.setSubscriptionType(stService.findSubscriptionTypeByName("Monthly").get(0));
+			inactiveSub.setValid(false);
+			inactiveSub.setValidFrom(new Date(Instant.now().toEpochMilli() - (MILIS_IN_A_DAY * 180)));
+			inactiveSub.setValidTo(new Date(Instant.now().toEpochMilli() - (MILIS_IN_A_DAY * 150)));
+			subService.saveSubscription(inactiveSub);
+
+
+			Collection<Subscription> userSubs = u.getSubscriptions();
+			userSubs.add(activeSub);
+			userSubs.add(inactiveSub);
+			u.setSubscriptions(userSubs);
+			userService.updateUser(u);
+		}
+	}
+
+	private void generateSubscriptionTypes() {
+		SubscriptionType type = new SubscriptionType();
+		type.setName("Daily");
+		type.setPrice(10F);
+		stService.saveSubscriptionType(type);
+
+		type = new SubscriptionType();
+		type.setName("Weekly");
+		type.setPrice(50F);
+		stService.saveSubscriptionType(type);
+
+		type = new SubscriptionType();
+		type.setName("Monthly");
+		type.setPrice(150F);
+		stService.saveSubscriptionType(type);
+
+		type = new SubscriptionType();
+		type.setName("Half Year");
+		type.setPrice(500F);
+		stService.saveSubscriptionType(type);
 	}
 
 	private void addRoles() {
