dfa3ccf2a26fa7d53afb07fd9ca35ec5b2ec98b0
Leo Jakus <leo.jakus@gmail.com>
2020-05-22 13:58:25 +0200

    Added partial Admin Rest tests

--- src/test/java/rest/AdminRestControllerTest.java
+++ src/test/java/rest/AdminRestControllerTest.java
@@ -7,15 +7,15 @@ import static rest.RestControllerTestUtility.*;
 
 public class AdminRestControllerTest extends AbstractTest {
 
-    private String updateCameraJSON = "{\"id\":1,\"name\":\"camUp\",\"videos\":[{\"id\":1,\"location\":\"/path1\"}]}";
+    private String updateCameraJSON = "{\"id\":1,\"name\":\"camUp\",\"videos\":[]}";
     private String nonExistingCameraJSON = "{\"id\":56789,\"name\":\"nonExistant\",\"videos\":[{\"id\":1,\"location\":\"/path1\"}]}";
     private String createCameraJSON = "{\"id\":\"\",\"name\":\"camName\",\"videos\":[{\"id\":null,\"location\":\"\"}]}";
     private String createInvalidCameraJSON = "{\"id\":\"\",\"name\":\"\",\"videos\":[{\"id\":,\"location\":\"\"}]}";
-    private String deleteCameraUrl = "/admin/delete/camera";
-    private String createLocationUrl = "/admin/create/location";
-    private String createCameraUrl = "/admin/create/camera";
-    private String getAllCamerasUrl = "/admin/get/cameras";
-    private String updateCameraUrl = "/admin/update/camera";
+    private String deleteCameraUrl = "/api/admin/deleteCamera";
+    private String createLocationUrl = "/api/admin/create/location";
+    private String createCameraUrl = "/api/admin/addCamera";
+    private String getAllCamerasUrl = "/api/admin/getCameras";
+    private String updateCameraUrl = "/api/admin/updateCamera";
 
 
     @BeforeMethod
@@ -35,59 +35,61 @@ public class AdminRestControllerTest extends AbstractTest {
     @Test
     public void createCameraTest() {
         int result = postWithToken(createCameraUrl, createCameraJSON, token);
-        assertTrue(result == statusCreated);
+        assertTrue(result == statusCreated, "Result is: " + result);
     }
 
     @Test
     public void createInvalidCameraTest() {
         int result = postWithToken(createCameraUrl, createInvalidCameraJSON, token);
-        assertTrue(result == statusFail);
+        assertTrue(result == statusFail, "Result is: " + result);
     }
 
     @Test
     public void doubleCreateCameraTest() {
         postWithToken(createCameraUrl, createCameraJSON, token);
         int result = postWithToken(createCameraUrl, createCameraJSON, token);
-        assertTrue(result == statusCreated); // should this fail?
+        assertTrue(result == statusCreated, "Result is: " + result); // should this fail?
     }
 
     @Test
     public void getAllCamerasTest() {
         postWithToken(createCameraUrl, createCameraJSON, token);
         String result = getRequestWithToken(getAllCamerasUrl, token).toString();
-        assertTrue(result.contains("camName")); //may be null
+        assertTrue(result.contains("camName"), "Result is: " + result); //may be null
     }
 
     @Test
     public void updateCameraTest() {
+        postWithToken(createCameraUrl, createCameraJSON, token);
         int result = postWithToken(updateCameraUrl, updateCameraJSON, token);
-        assertTrue(result == statusCreated);
+        assertTrue(result == statusCreated, "Result is: " + result);
     }
 
     @Test
     public void updateSameCameraTest() {
+        postWithToken(createCameraUrl, createCameraJSON, token);
         postWithToken(updateCameraUrl, updateCameraJSON, token);
         int result = postWithToken(updateCameraUrl, updateCameraJSON, token);
-        assertTrue(result == statusCreated);
+        assertTrue(result == statusCreated, "Result is: " + result);
     }
 
     @Test
     public void updateNonExistingCameraTest() {
         int result = postWithToken(updateCameraUrl, nonExistingCameraJSON, token);
-        assertTrue(result == statusCreated); //Should this fail?
+        assertTrue(result == statusCreated, "Result is: " + result); //Should this fail?
     }
 
     @Test
     public void deleteCameraTest() {
         postWithToken(createCameraUrl, createCameraJSON, token);
         int result = postWithToken(deleteCameraUrl, "1", token);
-        assertTrue(result == statusCreated);
+        assertTrue(result == statusCreated, "Result is: " + result);
     }
 
     @Test
     public void deleteNonExistingCameraUrlTest() {
         int result = postWithToken(deleteCameraUrl, "0", token);
-        assertTrue(result == statusFail); //if frontend breaks this should return false
+        assertTrue(result == statusFail, "Result is: " + result); //if frontend breaks this should return false
     }
 
     @Test
@@ -95,25 +97,34 @@ public class AdminRestControllerTest extends AbstractTest {
         postWithToken(createCameraUrl, createCameraJSON, token);
         postWithToken(deleteCameraUrl, "1", token);
         int result = postWithToken(deleteCameraUrl, "1", token);
-        assertTrue(result == statusFail);
+        assertTrue(result == statusFail, "Result is: " + result);
     }
 
     @Test
     public void deleteNothingTest() {
         int result = postWithToken(deleteCameraUrl, "", token);
-        assertTrue(result == statusFail);
-    }
-
-    @Test
-    public void createFalseLocationTest() {
-        int result = postWithToken(createLocationUrl, "", token);
-        assertTrue(result == statusFail);
+        assertTrue(result == statusFail, "Result is: " + result);
     }
 
     @Test
     public void createLocationTest() {
         int result = postWithToken(createLocationUrl, "camera", token);
-         assertTrue(result == statusCreated);
+         assertTrue(result == statusCreated, "Result is: " + result);
     }
+
+    //TODO
+//    /getAdminStatistics
+//    /addApplicationProperty
+//    /getApplicationProperty
+//    /addNewQuiz
+//    /getQuiz
+//    /updateQuiz
+//    /getNewQuizesForUser
+//    /getQuizesTakenByUser
+//    /addQuizAnswers
+//    /getAllAnswersForQuiz
+//    /getLocationWithWorkingHours
+//    /saveLocationWorkingHours
+    // VIDEO TEST HANDLED MANUALLY
 
 }
